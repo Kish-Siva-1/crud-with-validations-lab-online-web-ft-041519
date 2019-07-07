981309c134492cba15aef865ae989c0d76c7108c
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-07 13:19:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,15 +2,15 @@ class Song < ApplicationRecord
   validates :title, presence: true
   #validates_uniqueness_of :artist_name, scope: :release_year
   validates :release_year, presence: true, if: :released
-  #validate :date_limit
+  validate :date_limit
   validates :artist_name, presence: true
 
-  #private
+  private
 
-  #def date_limit
-  #  if release_year > Time.new.year
-  #    errors.add(:volume, "can't be released in the future")
-  #  end
-  #end
+  def date_limit
+    if release_year > Time.new.year
+      errors.add(:volume, "can't be released in the future")
+    end
+  end
 
 end
