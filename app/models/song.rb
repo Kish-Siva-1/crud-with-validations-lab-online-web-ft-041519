590644bfa73e0bad906114deeaaa4ded4007e455
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-07 09:25:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,16 @@
-class Song < ApplicationRecord
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates_uniqueness_of :artist_name, scope: :release_year
+  validates :release_year, presence: true, if: :released
+  #validate :date_limit
+  validates :artist_name, presence: true
+
+  #private
+
+  #def date_limit
+  #  if release_year > Time.new.year
+  #    errors.add(:volume, "can't be released in the future")
+  #  end
+  #end
+
 end
