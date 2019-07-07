df85b109b4b0cc5c3cc578024a6b22addc566674
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-07 13:30:04 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-  #validates_uniqueness_of :artist_name, scope: :release_year
+  validates_uniqueness_of :artist_name, scope: :release_year
   validates :release_year, presence: true, if: :released
   validate :date_limit
   validates :artist_name, presence: true
